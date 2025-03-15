dece9ff612dbb278570edead8e0126ba51d7b279
viniciusmgn <vinicius.marianog@gmail.com>
2025-03-15 20:29:36 -0300

    Changed main simulation environment

--- uaibot/simulation/simulation.py
+++ uaibot/simulation/simulation.py
@@ -170,7 +170,7 @@ class Simulation:
     #######################################
 
     def __init__(self, obj_list=[], ambient_light_intensity=12, ldr_urls=None, camera_type="perspective", width=[],
-                 height=[], show_world_frame = True, show_grid = True, load_screen_color="#19bd39", background_color="black",
+                 height=[], show_world_frame = True, show_grid = True, load_screen_color="#19bd39", background_color="#F5F6FA",
                  camera_start_pose = None):
 
         if not Utils.is_a_number(ambient_light_intensity) or ambient_light_intensity < 0:
@@ -206,7 +206,7 @@ class Simulation:
                 for url in ldr_urls:
                     error = Utils.is_url_available(url, ['png', 'bmp', 'jpg', 'jpeg'])
                     if not (error == "ok!"):
-                        raise Exception("The parameter 'url' " + error)
+                        raise Exception("The parameter 'url' #F5F6FA" + error)
 
         if camera_start_pose is None:
             if camera_type=="perspective":
@@ -239,7 +239,17 @@ class Simulation:
         self._load_screen_color = load_screen_color
         self._background_color = background_color
         self._camera_start_pose = np.array(camera_start_pose).tolist()
+   
+        light1 = PointLight(name="light1", color="white", intensity=4, htm=Utils.trn([-1,-1, 1.5]))
+        light2 = PointLight(name="light2", color="white", intensity=4, htm=Utils.trn([-1, 1, 1.5]))
+        light3 = PointLight(name="light3", color="white", intensity=4, htm=Utils.trn([ 1,-1, 1.5]))
+        light4 = PointLight(name="light4", color="white", intensity=4, htm=Utils.trn([ 1, 1, 1.5]))
         
+        self.add(light1)
+        self.add(light2)
+        self.add(light3)
+        self.add(light4)
+             
         #Add reference frame
         if self._show_world_frame:
             self.add(Frame(name='w0_frame', htm=Utils.trn([0,0,0]),size=0.5))
