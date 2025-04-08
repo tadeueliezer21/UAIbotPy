Corrected a bug in the Jacobian for prismatic joints

uaibot/robot/_fkm.py
@@ -58,7 +58,11 @@ def _fkm_python(self, q, axis, htm):
             htm_dh[i][:, :] = htm_dh[i][:, :] * Utils.rotz(self._links[i].theta)
 
         if self.links[i].joint_type == 1:
-            htm_dh[i][:, :] = htm_dh[i][:, :] * Utils.trn([0, 0, q[i]])
+            if isinstance(q, (np.matrix, np.ndarray)):
+                qi =  float(np.ravel(q)[i])
+            elif isinstance(q, list):
+                qi = float(q[i])
+            htm_dh[i][:, :] = htm_dh[i][:, :] * Utils.trn([0, 0, qi])
         else:
             htm_dh[i][:, :] = htm_dh[i][:, :] * Utils.trn([0, 0, self._links[i].d])
 

uaibot/robot/_jac_geo.py
@@ -53,9 +53,9 @@ def _jac_geo_python(self, q=None, axis='eef', htm=None):
 
 
     if axis == 'dh' or axis == 'eef':
-        htm_for_jac = self.fkm(q, 'dh', htm)
+        htm_for_jac = self.fkm(q, 'dh', htm, mode='python')
     if axis == 'com':
-        htm_for_jac = self.fkm(q, 'com', htm)
+        htm_for_jac = self.fkm(q, 'com', htm, mode='python')
 
     jac = [np.matrix(np.zeros((6,n))) for i in range(n)]
 
@@ -78,7 +78,7 @@ def _jac_geo_python(self, q=None, axis='eef', htm=None):
 
             if self.links[j].joint_type == 1:
                 jac[i][0:3, j] = z_j_ant
-                jac[i][3:6, j] = np.matrix(np.zeros((3,)))
+                jac[i][3:6, j] = np.matrix(np.zeros((3,1)))
 
     if axis == 'dh' or axis == 'com':
         return jac, htm_for_jac

uaibot/utils/utils.py
@@ -373,9 +373,9 @@ class Utils:
             rhs = np.concatenate((np.zeros((m,1)), b))
             solution = np.linalg.solve(M, rhs)
             
-            return np.matrix(solution[:m]).reshape((n,1))
+            return np.matrix(solution[:m]).reshape((m,1))
         else:
-            return np.matrix(ub_cpp.dp_inv_solve(A,b,eps)).reshape((n,1))
+            return np.matrix(ub_cpp.dp_inv_solve(A,b,eps)).reshape((m,1))
                  
 
     @staticmethod